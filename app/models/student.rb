38e36e04ecac1dddd84779f804fb6fa9bc3da69a
Allison Chesky <afc340@nyu.edu>
2017-06-13 01:31:28 +0000

    added has_many courses, slug, and find by slug to student class

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ActiveRecord
+  has_many :courses
+
+  def slug
+    self.username.downcase.gsub(",")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|inst| == inst.slug}
+  end
+
+
 end
