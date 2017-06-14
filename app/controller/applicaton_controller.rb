559303c9bf7c1fb86c450d25adb8aeb90cb71517
Allison Chesky <afc340@nyu.edu>
2017-06-14 00:17:47 +0000

    get / do index and post /signup directions

--- app/controller/applicaton_controller.rb
+++ app/controller/applicaton_controller.rb
@@ -8,4 +8,20 @@ class ApplicationController < Sinatra::Base
       enable :sessions
       set :session_secret, "bigbang"
     end
+
+
+    get '/' do
+      erb :index
+    end
+
+    post '/signup' do
+      @student = Student.create(params)
+      @session = session_secret
+      @session[:id] = @student.id
+      if is_logged_in? && !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+        redirect '/reviews'
+      else
+        redirect '/signup'
+      end
+    end
 end
